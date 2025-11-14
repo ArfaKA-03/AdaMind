af8c784649452f77b1c4584e3d246ef81bcabc04
Arfa K A <147141631+ArfaKA-03@users.noreply.github.com>
2025-11-14 19:32:26 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -14,7 +14,7 @@ function Signup({ onSignupSuccess }) {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${API_URL}/api/auth/signup`, {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
