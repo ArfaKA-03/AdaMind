e1843e5bf114acc8f86ee5228c744d791d6211b1
arrx <arrx31@gmail.com>
2025-12-07 19:56:50 +0530

    updating

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -13,7 +13,7 @@ function Signup({ onSignupSuccess }) {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch(`${process.env.FRONTEND_URL}/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
